26fc3bfdc7ad7eb8b073e378dae63cb2a51afe59
heather1138 <157194350+heather1138@users.noreply.github.com>
2024-02-19 20:09:02 -0700

    changing file path
    changing file path

--- streamlit_template/pages/kart_stats.py
+++ streamlit_template/pages/kart_stats.py
@@ -1,6 +1,26 @@
 import streamlit as st
+import pandas as pd 
 
 st.markdown("# Kart Configurations 🏎️")
 st.sidebar.markdown("# Kart Configurations 🏎️")
 
 st.write("What Kart Configuration is Best?")
+
+df_kart = pd.read_csv('data/kart_stats.csv')
+#st.write(df_kart)
+##st.dataframe(df_kart)
+##st.dataframe(df_kart)
+df_kart=df_kart[['Body','Weight','Acceleration','Mini-Turbo','Ground Speed','Water Speed']]
+
+st.dataframe(df_kart.style
+             .highlight_max(color='lightgreen',axis=0,subset=['Weight','Acceleration','Mini-Turbo','Ground Speed','Water Speed'])
+             .highlight_min(color='pink',axis=0,subset=['Weight','Acceleration','Mini-Turbo','Ground Speed','Water Speed'])
+             )
+st.line_chart(df_kart,x='Acceleration',y=['Mini-Turbo','Ground Speed'])
+st.scatter_chart(df_kart,x='Ground Speed',y=['Acceleration','Mini-Turbo'])
+#st.bar_chart(df_kart,x='Body',y=['Mini-Turbo'])
+chosen_kart = st.selectbox('Pick a Kart',df_kart['Body'])
+df_single_kart = df_kart.loc[df_kart['Body'] == chosen_kart]
+
+df_unp_kart= df_single_kart.unstack().rename_axis(['catagory','row_number']).reset_index().drop(columns='row_number').rename({0:'strength'},axis=1)
+st.bar_chart(df_unp_kart, x='catagory',y='strength')
